5a4acb37166ced6539bb5adf5699fc67d4d137fc
Albert <72774907+AlbertSimaoS@users.noreply.github.com>
2025-07-30 13:42:49 -0300

    Delete Desafio 16 directory

--- Desafio 16/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        TabuadaMultiplicacao numero1 = new TabuadaMultiplicacao();
-        numero1.mostrarTabuada(2);
-    }
-}
--- Desafio 16/TabuadaMultiplicacao.java
+++
@@ -1,9 +0,0 @@
-public class TabuadaMultiplicacao implements Tabuada{
-    @Override
-    public void mostrarTabuada(int numero) {
-        for (int i = 1; i <= 10; i++){
-            int valor = i * numero;
-            System.out.println(numero + "x" + i + " = " + valor);
-        }
-    }
-}
