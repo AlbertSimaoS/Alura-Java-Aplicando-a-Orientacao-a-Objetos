93f9c4d45f11bc97e1d3c34cdcfa89538861f38f
Albert <72774907+AlbertSimaoS@users.noreply.github.com>
2025-07-30 13:41:43 -0300

    Delete Desafio 10 directory

--- Desafio 10/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        ModeloCarro meuCarro = new ModeloCarro();
-        meuCarro.definirModelo("Sedan");
-        meuCarro.definirPrecos(30000, 32000, 35000);
-        meuCarro.exibirInfo();
-    }
-}
