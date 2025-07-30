7e11217650a673824933bcc424cdf59b0bad3433
Albert <72774907+AlbertSimaoS@users.noreply.github.com>
2025-07-30 14:02:56 -0300

    Delete Desafio 17 directory

--- Desafio 17/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        ConversorTemperaturaPadrao temperatura = new ConversorTemperaturaPadrao();
-        temperatura.celsiusParaFahrenheit(25);
-        temperatura.fahrenheitParaCelsius(75);
-    }
-}
