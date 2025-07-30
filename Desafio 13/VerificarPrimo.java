ea409ae1a77209d0aa5b8ef611e383bce1b23259
Albert <72774907+AlbertSimaoS@users.noreply.github.com>
2025-07-30 13:42:18 -0300

    Delete Desafio 13 directory

--- Desafio 13/VerificarPrimo.java
+++
@@ -1,9 +0,0 @@
-public class VerificarPrimo extends NumerosPrimos{
-    public void verificarSeEhPrimo(int numero) {
-        if (verificarPrimalidade(numero)) {
-            System.out.println(numero + " é primo.");
-        } else {
-            System.out.println(numero + " não é primo.");
-        }
-    }
-}
