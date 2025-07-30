0e73f0ae069fdd24c5f560cc2d6ade5fa0bed14e
Albert <72774907+AlbertSimaoS@users.noreply.github.com>
2025-07-30 13:42:04 -0300

    Delete Desafio 12 directory

--- Desafio 12/ContaCorrente.java
+++
@@ -1,8 +0,0 @@
-public class ContaCorrente extends ContaBancaria{
-    private double tarifaMensal;
-
-    public void cobrarTarifaMensal() {
-        saldo -= tarifaMensal;
-        System.out.println("Tarifa mensal de " + tarifaMensal + " cobrada. Saldo atual: " + saldo);
-    }
-}
