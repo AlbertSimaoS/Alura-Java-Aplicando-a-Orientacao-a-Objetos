b13b9fa3ef3560250c7f9e8e060fc063196d3f1a
Albert <72774907+AlbertSimaoS@users.noreply.github.com>
2025-07-30 14:03:24 -0300

    Delete Desafio 20 directory

--- Desafio 20/Titulo.java
+++
@@ -1,8 +0,0 @@
-public class Titulo implements Comparable<Titulo> {
-    private String nome;
-
-    @Override
-    public int compareTo(Titulo outroTitulo) {
-        return this.nome.compareTo(outroTitulo.nome);
-    }
-}
